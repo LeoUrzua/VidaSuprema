3e4f6abb08c98767ba28e1d902a646535dc2008f
leo <evolsignal@gmail.com>
2018-05-26 23:53:14 -0500

    Add simple list of articles

--- src/app/general/home/home.component.ts
+++ src/app/general/home/home.component.ts
@@ -9,7 +9,20 @@ import { ANIMATE_ON_ROUTE_ENTER } from '@app/core';
 export class HomeComponent implements OnInit {
 
   animateOnRouteEnter = ANIMATE_ON_ROUTE_ENTER;
-
+  articles = [
+    {
+      imageURL: 'http://files.constantcontact.com/d8b1e32b401/f95c984f-84aa-4a3d-a373-31a871ab66d7.jpg',
+      title: 'power'
+    },
+    {
+      imageURL: 'https://cdn-images-1.medium.com/max/1040/1*bpxmMpSKLs86DZWiP-_mnw@2x.png',
+      title: 'signal'
+    },
+    {
+      imageURL: 'http://www.theunheardnerd.com/wp-content/uploads/2016/05/Tron-Shanghai-Banner-640x300.jpg',
+      title: 'tron'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
--- src/app/general/single-article/single-article.component.ts
+++ src/app/general/single-article/single-article.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single-article.component.css']
 })
 export class SingleArticleComponent implements OnInit {
-
+  article = {
+    imageURL: 'http://files.constantcontact.com/d8b1e32b401/f95c984f-84aa-4a3d-a373-31a871ab66d7.jpg',
+    title: 'power'
+  };
   constructor() { }
 
   ngOnInit() {
