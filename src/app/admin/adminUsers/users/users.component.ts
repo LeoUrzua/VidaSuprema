7d95cf3dc7031424c8d94406983637859ae46df5
leo <evolsignal@gmail.com>
2018-05-29 17:34:44 -0500

    adding files to the last commit

---
+++ src/app/admin/adminUsers/users/users.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'admin/admin-users/users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  ngOnInit() {
+  }
+}
